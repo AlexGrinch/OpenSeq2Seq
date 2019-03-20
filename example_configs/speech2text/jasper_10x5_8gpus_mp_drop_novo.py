ed6a4dd664b22f69cceea6708e44d466ce2bb828
Aleksey Grinchuk (Oleksii Hrinchuk) <grinchuk.alexey@gmail.com>
2019-03-19 23:35:35 -0700

    Update jasper_10x5_8gpus_mp_drop_novo.py

--- example_configs/speech2text/jasper_10x5_8gpus_mp_drop_novo.py
+++ example_configs/speech2text/jasper_10x5_8gpus_mp_drop_novo.py
@@ -66,70 +66,70 @@ base_params = {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [11], "stride": [1],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.85,
+                "dilation":[1], "dropout_keep_prob": 0.9,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [11], "stride": [1],
                 "num_channels": 256, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.85,
+                "dilation":[1], "dropout_keep_prob": 0.9,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [13], "stride": [1],
                 "num_channels": 384, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.85,
+                "dilation":[1], "dropout_keep_prob": 0.9,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [13], "stride": [1],
                 "num_channels": 384, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.85,
+                "dilation":[1], "dropout_keep_prob": 0.9,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [17], "stride": [1],
                 "num_channels": 512, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.85,
+                "dilation":[1], "dropout_keep_prob": 0.9,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [17], "stride": [1],
                 "num_channels": 512, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.85,
+                "dilation":[1], "dropout_keep_prob": 0.9,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [21], "stride": [1],
                 "num_channels": 640, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.75,
+                "dilation":[1], "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [21], "stride": [1],
                 "num_channels": 640, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.75,
+                "dilation":[1], "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [25], "stride": [1],
                 "num_channels": 768, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.75,
+                "dilation":[1], "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
                 "type": "conv1d", "repeat": 5,
                 "kernel_size": [25], "stride": [1],
                 "num_channels": 768, "padding": "SAME",
-                "dilation":[1], "dropout_keep_prob": 0.75,
+                "dilation":[1], "dropout_keep_prob": 0.8,
                 "residual": True
             },
             {
